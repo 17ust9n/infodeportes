4378af4f666ff007710851b9098bd76251435c39
17ust9n <paolaverre@gmail.com>
2025-10-23 15:45:49 -0300

    Añadir equipos

--- appdeportes/models.py
+++ appdeportes/models.py
@@ -2,9 +2,42 @@ from django.db import models
 
 class Equipo(models.Model):
     nombre = models.CharField(max_length=100)
-    deporte = models.CharField(max_length=100)
-    ciudad = models.CharField(max_length=100)
-    fundado_en = models.IntegerField()
+
+    cantidad_hinchas = models.CharField(
+        max_length=100,
+        help_text="Cantidad de hinchas aproximadamente (puede no ser exacta)",
+        blank=True,
+        null=True
+    )
+    cantidad_copas = models.CharField(
+        max_length=100,
+        help_text="Cantidad de copas, títulos, veces que ganó el torneo, etc",
+        blank=True,
+        null=True
+    )
+    clasico_rival = models.CharField(
+        max_length=100,
+        help_text="Clásico rival y cuántos partidos le lleva",
+        blank=True,
+        null=True
+    )
+    apodo = models.CharField(
+        max_length=100,
+        help_text="Apodo del club, los hinchas y su estadio",
+        blank=True,
+        null=True
+    )
+    anio_fundacion = models.PositiveIntegerField(
+        help_text="Año de fundación del club",
+        blank=True,
+        null=True
+    )
+    escudo = models.ImageField(
+        upload_to='escudos/',
+        blank=True,
+        null=True,
+        help_text="Foto del escudo del club"
+    )
 
     def __str__(self):
         return self.nombre
--- infodeportes/urls.py
+++ infodeportes/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include  # include es para incluir URLs de la app
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),       # Ruta para el admin de Django
     path('', include('appdeportes.urls')), # Todas las demás URLs las maneja la app
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
